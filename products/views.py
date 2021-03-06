cc173ad214687725ee3cde20099a2db3fc32f160
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-03-06 15:38:58 +0000

    added in category sorting functionality, and product counts to product page

--- products/views.py
+++ products/views.py
@@ -19,16 +19,19 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
+
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -37,6 +40,7 @@ def all_products(request):
 
         if 'q' in request.GET:
             query = request.GET['q']
+
             if not query:
                 messages.error(
                     request, "You did not enter anys earch criteria")
