2f66b68d5ad40aea2aa132f9916939628ed617a4
Samuel Denton <samdenton1995@hotmail.co.uk>
2021-03-24 16:36:37 +0000

    Deleting product functionlaity added

--- products/views.py
+++ products/views.py
@@ -82,8 +82,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request, 'Failed to add product. Please ensure the form is valid')
@@ -119,3 +120,12 @@ def edit_product(request, product_id):
         'product': product
     }
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """Delete a product form the store"""
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(
+        request, f'Successfully deleted {product.name} from the store!')
+    return redirect(reverse('products'))
